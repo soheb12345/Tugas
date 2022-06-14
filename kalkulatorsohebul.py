8007ed35172b244f5c3bedd510d8ec49a770fab2
soheb12345 <107482412+soheb12345@users.noreply.github.com>
2022-06-14 21:14:41 +0700

    Add files via upload

---
+++ kalkulatorsohebul.py
@@ -0,0 +1,31 @@
+#Sohebul Bahri
+print('Kalkulator')
+print('1. Penjumlah')
+print('2. pengurang')
+print('3. perkalian')
+print('4. pembagian')
+
+operasi = input('pilih operasi (1|2|3|4): ')
+bilangan_1 = eval(input('Masukkan bilangan pertama: '))
+bilangan_2 =eval(input('Masukkan bilangan kedua: '))
+
+if operasi == '1':
+    print(f'hasilnya adalah: ',  (bilangan_1)+ (bilangan_2))
+elif operasi == '2':
+    print(f'hasilnya adalah: ', (bilangan_1)-(bilangan_2))
+elif operasi == '3':
+    print(f'hasilnya adalah: ', (bilangan_1)*(bilangan_2))
+elif operasi == '4':
+    print(f'hasilnya adalah: ', (bilangan_1)/(bilangan_2))
+else:
+    print('pikir sendiri')
+    
+class jawaban:
+    def _init_(self, jawaban):
+        self. jawaban = jawaban
+        
+class jawaban:
+    pass
+    ('jawaban ini benar') 
+    
+print('jawaban ini benar')                                  
